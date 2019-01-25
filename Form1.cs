6fc294e70da40d72c701396b7322d14b7b5d9459
Sadrak Gunadi <37262959+sadrakgunadi@users.noreply.github.com>
2019-01-25 21:35:56 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QRCodeGeneratorTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(textBox1.Text.Trim().ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(100, Color.Black, Color.White, true);
+
+            this.pictureBox1.Image = qrCodeImage;
+        } 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
